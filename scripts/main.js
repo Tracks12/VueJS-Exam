bd17b49fa06833e88791fbd3bb7ebf2d8850073f
Tracks12 <cardinal.florian.erwan@gmail.com>
2021-12-08 15:26:05 +0100

    Update UI and trash list

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 class store {
-  static load(data) {
-    return JSON.parse(atob(data));
-  }
+	static load(data) {
+		return JSON.parse(atob(data));
+	}
 
-  static save(data) {
-    return btoa(JSON.stringify(data));
-  }
+	static save(data) {
+		return btoa(JSON.stringify(data));
+	}
 }
 
 const toDoList = new XMLHttpRequest();
@@ -17,58 +17,81 @@ const appView = {
 
   methods: {
     addTask() {
-      if (!this.task) {
-        document.getElementById("task").focus();
+			if(!this.task) {
+				document.getElementById("task").focus();
+				return false;
+			}
 
-        return false;
-      }
+			let task = {
+				id: Date.now(),
+				label: this.task,
+				checked: false
+			};
 
-      let task = {
-        id: Date.now(),
-        label: this.task,
-      };
+			this.tasks.push(task);
+			this.task = null;
 
-      this.tasks.push(task);
-      this.task = null;
+			localStorage.tasks = store.save(this.tasks);
+		},
 
-      localStorage.tasks = store.save(this.tasks);
-    },
+		checkTask(task) {
+			task.checked = !task.checked;
 
-    delTask(task) {
-      this.tasks.forEach((v, k) => {
-        if (v.id === task.id) this.tasks.splice(k, 1);
+			localStorage.tasks = store.save(this.tasks);
+		},
 
-        localStorage.tasks = store.save(this.tasks);
-      });
-    },
+		editTask(task) {
+			console.log("edit task");
+		},
+
+		moveToTrash(task) {
+			this.tasks.forEach((v, k) => {
+				if(v.id === task.id) this.tasks.splice(k, 1);
+
+				localStorage.tasks = store.save(this.tasks);
+			});
+
+			this.trash.push(task);
+
+			localStorage.trash = store.save(this.trash);
+		},
+
+		undoMoveToTrash() {
+			this.tasks.push(this.trash[this.trash.length-1]);
+			this.trash.pop();
+
+			localStorage.trash = store.save(this.trash);
+			localStorage.tasks = store.save(this.tasks);
+		}
   },
 
-  data() {
-    return {
-      task: null,
-      tasks: localStorage.tasks ? store.load(localStorage.tasks) : [],
-    };
-  },
+	data() {
+		return {
+			task: null,
+			tasks: localStorage.tasks ? store.load(localStorage.tasks) : [],
+			trash: localStorage.trash ? store.load(localStorage.trash) : [],
+		};
+	},
 };
 
 window.onload = () => {
-  const app = Vue.createApp({
-    components: {
-      "app-view": appView,
-    },
+	const app = Vue.createApp({
+		components: {
+			"app-view": appView,
+		},
 
-    data() {
-      return {
-        app: {
-          title: "to do list",
-          version: "1.0",
-        },
-      };
-    },
-  });
+		data() {
+			return {
+				app: {
+					title: "to do list",
+					version: "1.0",
+				},
+			};
+		},
+	});
 
-  app.mount("#app");
-  //app.unmount();
+	app.mount("#app");
+	// app.unmount();
 };
 
 /**
